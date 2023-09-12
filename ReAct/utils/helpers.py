8f5740effa65785866ea7c68d90406e1efae894d
nee04 <neelgupta04@outlook.com>
2023-09-12 19:56:59 +0100

    Fixed it

--- ReAct/utils/helpers.py
+++ ReAct/utils/helpers.py
@@ -14,7 +14,7 @@ def save_eqx_obj(save_dir: str, filename: str, obj: tuple):
     eqx.tree_serialise_leaves(filename, obj)
         
 def load_eqx_obj(obj: tuple, filepath: str) -> tuple:
-    return eqx.tree_deserialise_leaves(filepath, obj)
+    return eqx.tree_deserialise_leaves(obj, filepath)
 
 def count_params(model: eqx.Module):
     num_params = sum(x.size for x in jax.tree_util.tree_leaves(eqx.filter(model, eqx.is_array)))
