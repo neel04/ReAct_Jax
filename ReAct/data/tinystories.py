42c80dd104b7071adb37a54aed265e6a60738a44
Neel Gupta <neelgupta04@outlook.com>
2023-10-05 23:26:26 +0100

    changing dataloader once again

--- ReAct/data/tinystories.py
+++ ReAct/data/tinystories.py
@@ -12,8 +12,8 @@ class TinyStoriesDataset:
         self.bsz = bsz
         self.max_length = max_length + 1
         
-        self.dataset = load_dataset('roneneldan/TinyStories', split=split, streaming=True,
-                                ignore_verifications=True, keep_in_memory=False)
+        self.dataset = load_dataset('roneneldan/TinyStories', split=split, streaming=False,
+                                    ignore_verifications=True, keep_in_memory=False, num_proc=os.cpu_count())
         
         self.tok = Tok(vocab_dir=vocab_dir, max_length=self.max_length)
 
