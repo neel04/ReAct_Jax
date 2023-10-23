Fixed weighting

ReAct/utils/helpers.py
@@ -56,7 +56,7 @@ def get_rand_nums(key: PRNGKeyArray, lower_bound: int, upper_bound: int, bsz: in
 def inverted_freq(arr: Array):
     values, counts = jnp.unique(arr, return_counts=True, size=64)
     counts = counts / counts.sum()
-    return 1 / jnp.clip(counts[arr - 1], 1.0)
+    return jnp.clip(1 / counts[arr - 1], 0, 256)
 
 if __name__ == '__main__':
     key = jax.random.PRNGKey(0)