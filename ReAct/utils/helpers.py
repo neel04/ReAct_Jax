Fixed n+k inverse weighting

ReAct/utils/helpers.py
@@ -52,11 +52,28 @@ def get_rand_nums(key: PRNGKeyArray, lower_bound: int, upper_bound: int, bsz: in
     random_numbers = jax.random.randint(key, shape=(bsz,), minval=lower_bound, maxval=upper_bound)
     return random_numbers
 
+def scale_array(arr, min_val=0, max_val=1):
+    arr_min, arr_max = jnp.min(arr), jnp.max(arr)
+    # You can rewrite this as jnp.where
+    return jax.lax.cond(jnp.all(arr_min == arr_max),
+                        lambda _: jnp.full(arr.shape, min_val),
+                        lambda _: (arr - arr_min) * (max_val - min_val) / (arr_max - arr_min) + min_val,
+                        operand=None)
+
 @jax.jit
 def inverted_freq(arr: Array):
-    values, counts = jnp.unique(arr, return_counts=True, size=64)
-    counts = counts / counts.sum()
-    return jnp.clip(1 / counts[arr - 1], 0, 256)
+    arr = arr.sort(0)
+    
+    values, counts = jnp.unique(arr,
+                                return_counts=True,
+                                size=64)
+    
+    # Replace 0s with any element for scaling to work
+    counts = jnp.where(counts == 0, counts[0], counts)
+    
+    inv_weights = scale_array((counts.max() / counts), 1.0, 5.0)
+    
+    return inv_weights[arr - arr.min()]
 
 if __name__ == '__main__':
     key = jax.random.PRNGKey(0)

ReAct/utils/trainer.py
@@ -53,11 +53,6 @@ def _compute_softmax_cross_entropy_loss(pred_y: Array, y_one_hot: Array, pad_mas
     
     weights = inverted_freq(n + k)
     
-    # if all the weights are same, then don't scale
-    # this is a workaround to avoid NaNs
-    weights = jnp.where(jnp.all(weights == weights[0]), weights, 
-                        (weights - weights.min()) / (weights.max() - weights.min()) * 4 + 1)
-    
     loss = (loss * weights).mean(-1)
     
     return loss.mean() # across all the batches
@@ -287,9 +282,9 @@ class Trainer:
                     self.my_logger.info(f'Validation accuracy: {val_metrics_5[0]} | using {self.max_iters + 5} iterations')
                     self.my_logger.info(f'Cumulative Training accuracy: {cum_train_acc}\n')
                     
-                    self.generate(model, sample_x, max_new_tokens=16)
+                    self.generate(model, sample_x, max_new_tokens=32)
                     self.my_logger.info(f'{"=" * 20}\tVal set prompt:\n')
-                    self.generate(model, val_sample_x, max_new_tokens=16)
+                    self.generate(model, val_sample_x, max_new_tokens=32)
                     
                     if step % self.save_interval == 0:
                         # Save the model 