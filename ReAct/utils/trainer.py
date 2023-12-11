73c0136128ec41abffba3de555bde1082197135a
Neel Gupta <neelgupta04@outlook.com>
2023-12-11 14:02:28 +0000

    enabled n+k loop

--- ReAct/utils/trainer.py
+++ ReAct/utils/trainer.py
@@ -26,14 +26,14 @@ from .helpers import get_rand_nums, half_precision
 @jax.jit
 def n_k_loop(model: eqx.Module, input_arr: Array, pad_mask: Array, n: int, k: int, key: PRNGKeyArray) -> Array:
     # forward pass the model without tracking grads
-    #output, intermediate_array = model(
-        #jax.lax.stop_gradient(input_arr), n,
-        #pad_mask=pad_mask, prev_thought=None, key=key)
+    output, intermediate_array = model(
+        jax.lax.stop_gradient(input_arr), n,
+        pad_mask=pad_mask, prev_thought=None, key=key)
     
-    #_, intermediate_array = jax.lax.stop_gradient(output), jax.lax.stop_gradient(intermediate_array)
+    _, intermediate_array = jax.lax.stop_gradient(output), jax.lax.stop_gradient(intermediate_array)
     
     # n-k passes, but track the gradient this time
-    output, _ = model(input_arr, n, pad_mask=pad_mask, prev_thought=None, key=key)
+    output, _ = model(input_arr, n, pad_mask=pad_mask, prev_thought=intermediate_array, key=key)
 
     return output
 
@@ -57,7 +57,7 @@ def _compute_softmax_cross_entropy_loss(pred_y: Array, y_one_hot: Array, pad_mas
     n = jnp.repeat(n[:, None], loss.shape[1], axis=-1)
     k = jnp.repeat(k[:, None], loss.shape[1], axis=-1)
 
-    loss = (loss * n).sum(-1) # across the sequence
+    loss = (loss * k).sum(-1) # across the sequence
     
     return loss.mean() # across all the batches
     
