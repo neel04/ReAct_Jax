Fix not averaging the train_acc across a batch

ReAct/utils/trainer.py
@@ -348,7 +348,7 @@ class Trainer:
                     
                     if trial is not None:
                         loss = jnp.nan_to_num(loss, nan=9999.0) # handle NaNs
-                        trial.report(train_acc, step)
+                        trial.report(sum(train_acc) / len(train_acc), step)
                         
                         if trial.should_prune() or jnp.isnan(loss):
                             raise optuna.exceptions.TrialPruned()