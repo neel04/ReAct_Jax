85e9c225ba738876432b537c5a51950ced56e1b6
Neel Gupta <neelgupta04@outlook.com>
2024-09-05 01:03:01 +0100

    `donate`-ing everything to save on VRAM

--- ReAct/utils/trainer.py
+++ ReAct/utils/trainer.py
@@ -72,7 +72,7 @@ def _compute_softmax_cross_entropy_loss(pred_y: Array, y_one_hot: Array) -> Arra
 
     return loss.mean()
 
-@eqx.filter_jit
+@eqx.filter_jit(donate='all')
 def make_step(
     model: Union[React, GPT],
     opt_state: PyTree,
@@ -412,7 +412,7 @@ class Trainer:
                         self.my_logger.warning(f'\nLoss is NaN at step {step}')
                         return loss
 
-                if (step + 1) % self.args.log_interval == 0 and len(train_acc) > 0:
+                if step % self.args.log_interval == 0 and len(train_acc) > 0:
                     # Compute cumulatives
                     cum_train_acc = sum(train_acc) / len(train_acc)
                     cum_train_loss = sum(train_loss) / len(train_loss)
