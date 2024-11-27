8cb33ed460d37b153a2b05353eec31c4d5f5c143
Neel Gupta <neelgupta04@outlook.com>
2024-11-27 23:22:47 +0000

    Added `uv` stuff

--- run_tpu.sh
+++ run_tpu.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -e
 BRANCH="dev"
-IMAGE_NAME="docker.io/neel04/react_image:latest"
-CONTAINER_NAME="react_container"
-RAMDISK_PATH=$(pwd)/ReAct_Jax/ramdisk/
+
+# Export environment variables
+export jax_threefry_partitionable=1
+export WANDB_API_KEY=78c7285b02548bf0c06dca38776c08bb6018593f
+export HF_TOKEN=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
+export HF_HOME=/tmp/
+export HF_DATASETS_CACHE=/tmp/
+export JAX_TRACEBACK_FILTERING=off
 
 # arguments for train_model.py
-TRAIN_ARGS="--save_dir ./ReAct/outputs/ --dataset 'owt' --group 'owt' \
+TRAIN_ARGS="--save_dir ./ReAct/outputs/ --dataset 'tinystories' --group 'owt' \
 --num_blocks 8 --width 128 --n_heads 16 --epochs 1 --num_classes 50304 \
 --log_interval 750 --save_interval 10000 --seqlen 512 \
 --max_iters 3 --batch_size 128 --accum_steps 4 \
@@ -15,48 +20,41 @@ TRAIN_ARGS="--save_dir ./ReAct/outputs/ --dataset 'owt' --group 'owt' \
 --weight_decay 5e-4 --drop_rate 0.00 --nesterov \
 --exp_logging --tune_hyperparams"
 
-# Stop all running Docker containers
-echo "Stopping all running Docker containers..."
-sudo docker stop $CONTAINER_NAME
-sudo docker rm $CONTAINER_NAME
-
-if ! sudo sh -c "timeout 150s docker rm -f $CONTAINER_NAME"; then
-    echo "Command timed out. Restarting Docker daemon & retrying..."
-    sudo systemctl restart docker
-    sleep 10s; sudo docker rm -f $CONTAINER_NAME
-fi
-
-# Git stuff
 git clone -b $BRANCH https://github.com/neel04/ReAct_Jax.git
 
-sudo -s <<EOF
+FLAG_FILE="./env_flag"
+
 git config --global safe.directory '*'
 cd ReAct_Jax/; git pull --all; git checkout f55d7bcc7d5243c66015d0bcab0e38946ad1b9dc; cd ..
 
-# Run the Docker container
-echo "Running Docker container..."
-docker run --ulimit core=0 --pull 'always' -v $(pwd)/ReAct_Jax/:/ReAct_Jax/ --mount type=tmpfs,destination=$RAMDISK_PATH,tmpfs-size=375000000000 -e HF_HOME=$RAMDISK_PATH -e HF_DATASETS_CACHE=$RAMDISK_PATH -e WANDB_API_KEY=78c7285b02548bf0c06dca38776c08bb6018593f -e HF_TOKEN=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 -e JAX_TRACEBACK_FILTERING=off -e LIBTPU_INIT_ARGS="--xla_tpu_impure_oom_fast_exit_threshold=-1" -e XLA_FLAGS="--xla_dump_to=dump/hlo-dump" --privileged --rm --net=host --name $CONTAINER_NAME -it -d $IMAGE_NAME
+if [ ! -f "$FLAG_FILE" ]; then
+    echo "Setting up environment..."
+    apt-get update
+    apt-get install neovim tmux
 
-# Get docker container ID to copy files
-CONTAINER_ID=$(docker ps -aqf "name=$CONTAINER_NAME")
-docker cp $(pwd)/ReAct_Jax $CONTAINER_ID:/
-export JAX_TRACEBACK_FILTERING=off
+    # Create virtual environment
+    pip3 install uv
+    uv venv 'main_env' --python 3.11
+    source main_env/bin/activate
 
-# Execute train_model.py inside the Docker container
-echo "Executing train_model.py inside Docker container..."
-docker exec --privileged $CONTAINER_NAME git config --global safe.directory '*'
-docker exec --privileged $CONTAINER_NAME python3 train_model.py $TRAIN_ARGS
+    uv pip install -U -q jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+    uv pip install -q transformers datasets scalax tokenizers icecream wandb einops torch tqdm jaxtyping optax optuna equinox rich
+    uv pip install -U optuna-integration plotly lm-eval pdbpp
+    uv pip install git+https://github.com/deepmind/jmp
+    uv pip install git+https://github.com/Findus23/jax-array-info.git
+    uv pip install -q tensorboard-plugin-profile tensorboard etils importlib_resources "cloud-tpu-profiler>=2.3.0"
 
-echo "Attempting graceful shutdown of Docker container..."
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
-
-# If graceful shutdown fails, force removal
-if [ "$(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME)" == "true" ]; then
-    echo "Forcefully removing Docker container..."
-    docker rm -f $CONTAINER_NAME
+    # ------------------
+    # Create the flag file
+    touch "$FLAG_FILE"
+else
+    echo "Reusing existing venv..."
 fi
 
-EOF
+echo "Executing train_model.py"
+source main_env/bin/activate
+
+echo "Executing train_model.py inside uv venv..."
+python3 train_model.py $TRAIN_ARGS
 
 echo "Finished training!"
