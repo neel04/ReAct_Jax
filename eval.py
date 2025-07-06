53be2bb19539e6872559f5b322193055c120e96f
Neel Gupta <neelgupta04@outlook.com>
2025-07-06 13:22:22 +0100

    [FIX?] `boostrap_iters=0` to disable it entirely

--- eval.py
+++ eval.py
@@ -256,6 +256,7 @@ class Evaluator:
             tasks=[self.task],
             num_fewshot=None,
             task_manager=task_manager,
+            bootstrap_iters=0
         )
 
         return results["results"]  # type: ignore
