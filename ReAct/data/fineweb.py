fa7ea8817dd948aedc304d6320b11a4a3914bdf2
Neel Gupta <neelgupta04@outlook.com>
2025-12-26 02:16:46 +0000

    Revert "Upgrading fineweb to 350B tokens"
    This reverts commit 85e821b4172555fb0573f1ed1d81c5e356157fe5.

--- ReAct/data/fineweb.py
+++ ReAct/data/fineweb.py
@@ -17,7 +17,7 @@ class FineWebDataset(ParentDataset):
             hf_username="Neel-Gupta",
             hf_dataset="fineweb",
             tgt_hf_repo="HuggingFaceFW/fineweb",
-            hf_subset_name="sample-350BT",
+            hf_subset_name="sample-100BT",
             max_length=seqlen,
             bsz=batch_size,
         )
@@ -43,13 +43,13 @@ class FineWebDataset(ParentDataset):
     ):
         """
         Override parent method to use streaming=True and implement train/test split
-        using take/skip for the massive FinewWeb dataset (~80B tokens).
+        using take/skip for the massive FinewWeb dataset (~100B tokens).
 
         For eval, we use ~1% of data (~1B tokens) which should be sufficient.
         """
         # No need for data preprocessing on non-primary processes
         if jax.process_index() != 0:
-            total_batches = 518458467 // self.bsz  # type: ignore
+            total_batches = 147639585 // self.bsz
             eval_samples = int(total_batches * 0.01)
 
             if split == "train":
