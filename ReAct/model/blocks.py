7e88dbb6bdea7f83dea3e846d7eb0060e9df16b9
Neel Gupta <neelgupta04@outlook.com>
2025-12-30 12:14:38 +0000

    removed switch

--- ReAct/model/blocks.py
+++ ReAct/model/blocks.py
@@ -422,7 +422,7 @@ class UnsharedBlock(eqx.Module, Generic[L]):
 
         with jax.ensure_compile_time_eval():
             branches = tuple(apply_fn(i) for i in range(len(layers)))
-            return jax.lax.switch(iteration_index, branches, args)
+            return branches[iteration_index](args)
 
 class NDRAttentionBlock(eqx.Module):
     """CopyGated Augmented block inspired by Csordas et al."""
